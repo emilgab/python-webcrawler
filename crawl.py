4b19e3c46fbd0dcb435bc8e375ecd172a18efe0e
Emil Gabrielli <emilgabrielli@gmail.com>
2019-12-06 00:55:59 +0100

    Added the Crawl object
    This object will store the starting domain to crawl and contain methods that will crawl this for us.

--- crawl.py
+++ crawl.py
@@ -8,6 +8,22 @@ import os
 import re
 import datetime
 
-# The script will store time data on when it was run.
-# This variable will also be used as filename for the crawl result
-run_date_and_time = '{0:%d-%m-%Y--%H:%M:%S}'.format(datetime.datetime.now())
+class Crawl():
+    '''
+    Crawl object which stores the site and methods related to crawling.
+    '''
+    # The object will store time data on when it was run.
+    # This variable will  be used as filename for the crawl result and for the print statement
+    run_date_and_time = '{0:%d-%m-%Y--%H:%M:%S}'.format(datetime.datetime.now())
+
+    def __init__(self, starting_url):
+        self.starting_url = starting_url
+
+    def __str__(self):
+        return "\nStarting domain crawled: %s \nDate and time: %s\n" % (self.starting_url, self.run_date_and_time)
+
+    def crawlNext(self):
+        pass
+
+x = Crawl(input("What website do you wish to crawl? "))
+print(x)
