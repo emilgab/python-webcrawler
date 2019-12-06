c791ebdb82765fae207ba49a0866e147fefeccbb
Emil Gabrielli <emilgabrielli@gmail.com>
2019-12-06 02:53:29 +0100

    Added the welcome function
    This function will print out a welcome message with some info about the programy

--- webcrawler.py
+++ webcrawler.py
@@ -4,9 +4,28 @@
 
 # imports
 import sys
+import os
 from crawler.crawler import Crawl
 
-print("What website would you like to crawl?")
-print("NOTE that the URL have to include 'http(s)://''")
-x = Crawl(input("URL: "))
-print(x)
+def welcome():
+    rows, columns = os.popen('stty size','r').read().split()
+
+    decoration = '--+ ~ * ~ +--'.center(int(columns))
+    welcome_msg = "Welcome to this Python web crawler!".center(int(columns))
+    credit = "Created by Emil Gabrielli".center(int(columns))
+    course_inf = "for OsloMet course ACIT4420: Problem-Solving with scripting".center(int(columns))
+
+    print("\n"*int(rows))
+    print(decoration)
+    print(welcome_msg)
+    print(credit)
+    print(decoration)
+    print("\n"*(int(rows)//5))
+    print("What website would you like to crawl?".center(int(columns)))
+    print("NOTE: the URL must include 'http(s)://''".center(int(columns)))
+    print("\n"*(int(rows)//5))
+    x = Crawl(input("URL: "))
+    print(x)
+
+if __name__ == '__main__':
+    welcome()
